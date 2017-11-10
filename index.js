44510d576eef160e7319fa74c9ef6966b0a2a7eb
Stephanie McSpedon-Marx <s.mcspedon@gmail.com>
2017-11-10 17:50:28 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,10 +1,3 @@
 // Write your solution in this file!
 var customerName = 'bob';
 
-function upperCaseCustomerName {
-  customerName = customerName.toUpperCase();
-}
-
-function setBestCustomer {
-  const bestCustomer = 
-}
